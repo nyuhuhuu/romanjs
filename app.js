1f0b493eb8f521315c634a4527e69b4135b0ba51
Gabor Torok <gtorok@babbel.com>
2018-06-12 09:15:33 +0200

    Add solution to roman converter

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 function roman(n) {
+
+    const rules = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+
+    let roman = "";
+    for (let sym in rules) {
+        let div = Math.floor(n / rules[sym]);
+        roman += sym.repeat(div);
+        n = n % rules[sym];
+    }
+    return roman;
+
 }
 
 module.exports = roman;
